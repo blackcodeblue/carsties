2aa5809bddf8c96529097319b6215ac44d71069d
kevin mcmillan <kevin@blackcodeblue.com>
2023-12-29 19:31:38 -0600

    grpc calls and creating/dockerizing new bid service

--- src/AuctionService/Consumers/BidPlacedConsumer.cs
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AuctionService.Data;
 using Contracts;
 using MassTransit;
@@ -20,14 +16,14 @@ public class BidPlacedConsumer : IConsumer<BidPlaced>
     {
         Console.WriteLine("--> Consuming bid placed.");
 
-        var auction = await _dbContext.Auctions.FindAsync(context.Message.Id);
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId));
 
         if (auction.CurrentHighBid == null
             || context.Message.BidStatus.Contains("Accepted")
             && context.Message.Amount > auction.CurrentHighBid)
         {
             auction.CurrentHighBid = context.Message.Amount;
-            
+
             await _dbContext.SaveChangesAsync();
         }
     }
